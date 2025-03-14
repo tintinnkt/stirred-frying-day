a55a6b372969e8461b140a8c92e237259705d98b
tintinnkt <t.tintinnkt@gmail.com>
2025-03-14 10:14:49 +0700

    feat: initial courses and planners page with apiRoutes config

---
+++ src/app/(planner)/planners/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <main className="w-full md:max-w-3/5 bg-neutral-50 justify-self-center">
+      <h2>PROG METH</h2>
+    </main>
+  );
+};
+
+export default page;
---
+++ src/conifg/apiRoutes.ts
@@ -0,0 +1,7 @@
+export enum BackendRoutes {}
+export enum FrontendRootRoutes {
+  ADMIN = "/admin",
+  COURSE = "/courses",
+  PLANNER = "/planners",
+}
+export enum FrontendRoutes {}
