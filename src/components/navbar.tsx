ae535d64309ef3d3c26566502d7b24effda0738a
tintinnkt <t.tintinnkt@gmail.com>
2025-03-18 16:53:15 +0700

    feat: add menu in navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,14 +1,51 @@
 "use client";
-import { Menu } from "lucide-react";
+import { FrontendRoutes } from "@/conifg/apiRoutes";
+import {
+  BookOpenTextIcon,
+  HomeIcon,
+  MenuIcon,
+  NotebookPenIcon,
+} from "lucide-react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 
 const Navbar = () => {
   const router = useRouter();
   return (
     <div className="flex w-full flex-wrap items-center justify-between bg-amber-300 p-4">
       <Image src={"/logo.png"} alt="" width={40} height={40}></Image>
-      <Menu />
+      <DropdownMenu>
+        <DropdownMenuTrigger className="rounded-md p-1 transition-all hover:scale-105 hover:bg-gray-100/40">
+          <MenuIcon />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56">
+          <DropdownMenuLabel>Login | My Profile</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuLabel onClick={() => router.push("/")}>
+            <HomeIcon />
+            <span>Home</span>
+          </DropdownMenuLabel>
+          <DropdownMenuLabel
+            onClick={() => router.push(FrontendRoutes.COURSE_LIST)}
+          >
+            <BookOpenTextIcon />
+            <span>Subjects</span>
+          </DropdownMenuLabel>
+          <DropdownMenuLabel
+            onClick={() => router.push(FrontendRoutes.PLANNER)}
+          >
+            <NotebookPenIcon />
+            <span>Planners</span>
+          </DropdownMenuLabel>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 };
--- src/conifg/apiRoutes.ts
+++ src/conifg/apiRoutes.ts
@@ -1,7 +1,12 @@
 export enum BackendRoutes {}
 export enum FrontendRootRoutes {
   ADMIN = "/admin",
-  COURSE = "/courses",
+  LIST = "/courses",
   PLANNER = "/planners",
+  LOGIN = "/login",
 }
-export enum FrontendRoutes {}
+export enum FrontendRoutes {
+  ADMIN_MANAGEMENT = `${FrontendRootRoutes.ADMIN}/management`,
+  COURSE_LIST = `${FrontendRootRoutes.LIST}`,
+  PLANNER = `${FrontendRootRoutes.PLANNER}`,
+}
